edc64293474855e33c2820e5a60a0a532d2fdb4d
Darren Siegel <siegel.darren@gmail.com>
2019-03-02 17:32:14 -0500

    raw data, setup and gitignore

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Sets up the project environment
+
+# Unzips the data if not present
+if [ ! -d "./data" ]; then
+  echo "Unzipping UCR data"
+  mkdir "./data"
+  unzip "./archives/ucr.zip" -d "./data"
+fi
+
+# Installs the dependencies
